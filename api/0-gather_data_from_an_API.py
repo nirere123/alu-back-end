3d7d01a41b32baaec83061b72a01170b3ac392f7
nirere123 <n.sayinzoga@alustudent.com>
2025-11-06 01:49:31 -1000

    changes

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -6,28 +6,28 @@ Module for fetching employee TODO list progress from REST API
 import requests
 import sys
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     employee_id = sys.argv[1]
 
-    # Get user info
+    # Get employee info
     user = requests.get(
         f"https://jsonplaceholder.typicode.com/users/{employee_id}"
     ).json()
     employee_name = user.get("name")
 
-    # Get TODO list
+    # Get the employee's todo list
     todos = requests.get(
-        f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+        f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
     ).json()
 
-    # Filter completed tasks
-    completed_tasks = [
-        task.get("title") for task in todos if task.get("completed")
-        ]
+    # Count completed tasks
+    completed_tasks = [task for task in todos if task.get("completed")]
 
-    # Print output
+    # Print in the required format
     print("Employee {} is done with tasks({}/{}):".format(
         employee_name, len(completed_tasks), len(todos))
     )
+
     for task in completed_tasks:
-        print("\t {}".format(task))
+        print("\t {}".format(task.get("title")))
